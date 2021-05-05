5b6465a8a09526a782ea34747bcc033e04282c9c
Valdo227 <59632809+Valdo227@users.noreply.github.com>
2021-05-04 20:57:48 -0500

    fix(index,script)

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,16 +1,65 @@
+
 function Calculadora(){
-    var ec = document.toString().getElementById("input");
 
-    if(ec.includes("+","-","*","/")){
-
-    }else
-        alert("Los valores no pueden ser cero");
-    for(let i=0;i<ec.length;i++)
-    {
-
+    let ec = document.getElementById("input").value;
+    var res = 0;
+    if(ec.includes("+")){
+        res = sumar(ec);
     }
-    document.getElementById("res").innerHTML="";
-    document.getElementById("res").innerHTML= ec;
+    else if(ec.includes("-")){
+        res = restar(ec);
+    
+    }
+    else if(ec.includes("*")){
+        res = multiplicar(ec);
+    }
+    else if(ec.includes("/")){
+        res = dividir(ec);
+    }
+    else
+        alert("Ingresa un operador + , - , * , /");
 
+
+    document.getElementById("res").innerHTML="";
+    document.getElementById("res").innerHTML= res;
 
 }
+
+function numbers(ec,op){
+    let num = [0];
+    let pos = ec.indexOf(op);
+
+    if(pos === ec.lastIndexOf(op)){
+
+        num[0] = parseInt(ec.substring(0,pos));
+        num[1] = parseInt(ec.substring(pos+1,ec.length));
+        console.log(num);
+    }
+    else
+        alert("El operador se repite")
+
+    return num;
+}
+
+function sumar(ec){
+    let num = numbers(ec,"+");
+    return num[0]+num[1];
+}
+
+function restar(ec){
+    let num = numbers(ec,"-");
+    return num[0]-num[1];
+}
+
+function multiplicar(ec){
+    let num = numbers(ec,"*");
+    return num[0]*num[1];
+}
+
+function dividir(ec){
+    let num = numbers(ec,"/");
+    return num[0]/num[1];
+
+}
+
+
