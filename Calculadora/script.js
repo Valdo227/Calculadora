4a2b4d84c2337f2909802b709c54f486db86e668
Valdo227 <59632809+Valdo227@users.noreply.github.com>
2021-05-04 19:25:36 -0500

    feat(index,script)

---
+++ Calculadora/script.js
@@ -0,0 +1,16 @@
+function Calculadora(){
+    var ec = document.toString().getElementById("input");
+
+    if(ec.includes("+","-","*","/")){
+
+    }else
+        alert("Los valores no pueden ser cero");
+    for(let i=0;i<ec.length;i++)
+    {
+
+    }
+    document.getElementById("res").innerHTML="";
+    document.getElementById("res").innerHTML= ec;
+
+
+}
